66ea0a9c6d8b8efa8a9529555d88067a5eda5301
tync0 <isa.quluyev.wip@gmail.com>
2024-03-26 09:46:20 +0400

    Pycodestyle Validation - state.py

--- models/state.py
+++ models/state.py
@@ -16,7 +16,7 @@ class State(BaseModel, Base):
         """ Initialization of State instance """
         super().__init__(*args, **kwargs)
 
-    if getenv('HBNB_TYPE_STORAGE') != 'db': 
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             """Getter Function for FileStorage"""

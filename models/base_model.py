77cd686120db3835534a269f8d1628fc5e96aa5f
tync0 <isa.quluyev.wip@gmail.com>
2024-03-26 09:37:02 +0400

    Now, id, created_at, updated_at can be created only in __init__

--- models/base_model.py
+++ models/base_model.py
@@ -18,16 +18,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
+            kwargs['updated_at'] = self.updated_at
+            kwargs['created_at'] = self.created_at
+            try:
+                del kwargs['__class__']
+            except KeyError:
+                pass
             self.__dict__.update(kwargs)
 
     def __str__(self):

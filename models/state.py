7cb9897416d76f684f2022bc979667d9fea05f90
tync0 <isa.quluyev.wip@gmail.com>
2024-03-26 07:36:57 +0400

    Sqlalchemy columns defined for State class

--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete', backref='state')
+
+    def __init__(self, *args, **kwargs):
+        """ Initialization of State instance """
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db': 
+        @property
+        def cities(self):
+            """Getter Function for FileStorage"""
+            from models import storage
+            result = []
+            all_cities = storage.all(City)
+            for city in all_cities:
+                if city.state_id == self.id:
+                    result.append(city)
+            return result

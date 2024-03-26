169bfa454b70f3e646ac1fad0b1f362661d8ec60
tync0 <isa.quluyev.wip@gmail.com>
2024-03-26 09:39:23 +0400

    Many-to-Many relationship implemented

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 from models.review import Review
@@ -22,6 +22,14 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
     reviews = relationship('Review', cascade='all, delete', backref='place')
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60), ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
+
+    amenities = relationship('Amenity', secondary='place_amenity',
+                             viewonly=False)
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
@@ -29,8 +37,25 @@ class Place(BaseModel, Base):
             """Getter Function for FileStorage"""
             from models import storage
             result = []
-            all_reviews = storage.all(Review)
+            all_reviews = storage.all(Review).values()
             for review in all_reviews:
                 if review.place_id == self.id:
                     result.append(review)
             return result
+
+        @property
+        def amenities(self):
+            """Getter Function for FileStorage"""
+            from models import storage
+            result = []
+            all_amenities = storage.all(Amenity).values()
+            for amenity in all_amenities:
+                if amenity.id in self.amenity_ids:
+                    result.append(amenity)
+            return result
+
+        @amenities.setter
+        def amenities(self, obj):
+            """ Setter Function for FileStorage"""
+            if type(obj) is Amenity:
+                self.amenity_ids.append(obj.id)

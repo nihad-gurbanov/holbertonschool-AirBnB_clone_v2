26b1781fc952776342d8b0194716e555b61e5d34
tync0 <isa.quluyev.wip@gmail.com>
2024-03-26 09:46:02 +0400

    Pycodestyle Validation - place.py

--- models/place.py
+++ models/place.py
@@ -23,9 +23,11 @@ class Place(BaseModel, Base):
     amenity_ids = []
     reviews = relationship('Review', cascade='all, delete', backref='place')
     place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60), ForeignKey('places.id'),
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
                                  primary_key=True, nullable=False),
-                          Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
                                  primary_key=True, nullable=False))
 
     amenities = relationship('Amenity', secondary='place_amenity',
